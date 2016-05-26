3eb6c2f0b70ae2c5b6f264385b88242a92d699d1
LoginovIlya <LoginovIlya@users.noreply.github.com>
2016-05-26 19:33:52 +0300

    Delete ruby code

--- lib/enginedata/errors.php
+++ lib/enginedata/errors.php
@@ -1,11 +1,3 @@
 <?php
 
 class TokenNotFound extends  Exception{}
-
-
-//# -*- encoding : utf-8 -*-
-//class PSD
-//  class EngineData #:nodoc:
-//    class TokenNotFound < StandardError; end
-//  end
-//end
--- lib/enginedata/node.php
+++ lib/enginedata/node.php
@@ -14,14 +14,3 @@ class Node{
 }
 
 
-// # -*- encoding : utf-8 -*-
-// class PSD
-//   class EngineData
-//     # Represents a single node in the parsing tree. Extended with Hashie methods
-//     # to make data access easier.
-//     class Node < Hash
-//       include Hashie::Extensions::MergeInitializer
-//       include Hashie::Extensions::MethodAccess
-//     end
-//   end
-// end
